1dc07bfc275cfe23d6e13350584fefd5d426dc70
YousefElghanam <yousef.elghanam@gmail.com>
2025-11-23 14:16:45 +0100

    ADDED: ans_freq[], basic display_map() no colors

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
-use std::io:: Read;
+use std::{array, io::Read};
 use std::fs::File;
 use rand_set::RandSet;
 use colored::*;
 use rustyline::{DefaultEditor, Result, error::ReadlineError};
+
 
 fn  load_dict() -> RandSet<String> {
 	let mut	dict: RandSet<String> = RandSet::new();
@@ -32,23 +33,55 @@ fn  load_dict() -> RandSet<String> {
 	dict
 }
 
-fn	display_guesses(answer: String, guess: String) {
-	// ADD LOGIC HERE
+fn	fill_ans_freq (answer: String, mut ans_freq: [i32; 26]) -> [i32; 26] {
+	for c in answer.bytes() {
+		if c.is_ascii_alphabetic() {
+			ans_freq[(c.to_ascii_lowercase() - b'a') as usize] += 1;
+		}
+	}
+	ans_freq
 }
+
+fn	print_ans_freq (ans_freq: [i32; 26]) {
+	for (i, freq) in ans_freq.iter().enumerate() {
+		println!("{}: {}", (b'a' + i as u8) as char, freq)
+	}
+}
+
+fn	display_map(answer: String, ans_freq: [i32; 26], map: &Vec<String>) {
+	for i in 0..6 {
+		if map[i].is_empty() {
+			println!("_____");
+		}
+		else {
+			println!("{}", map[i]);
+		}
+	}
+}
+
 
 fn  main() -> Result<()> {
 	let mut	rl = DefaultEditor::new()?;
 	let		dict: RandSet<String>;
 	let		answer;
-	let mut	tries = 6;
+	let mut	tries = 0;
+	let mut	ans_freq = [0; 26];
+	let mut	map = vec![String::new(); 6];
 
 	dict = load_dict();
 	answer = dict.get_rand();
+	ans_freq = fill_ans_freq(answer.unwrap().to_string(), ans_freq);
+	
+	/* DEBUG */
+	// print_ans_freq(ans_freq);
+
 	println!("== Wordle ==");
 	println!("Guess the word");
+
+	/* DEBUG */
 	println!("looking for {}. shshshs :D", answer.unwrap().green());
 
-	// display_guesses(&answer, NULL);
+	display_map(answer.unwrap().to_string(), ans_freq, &map);
 	loop {
 		let	readline = rl.readline(&format!("{}", ">> ".green().bold()));
 
@@ -66,20 +99,19 @@ fn  main() -> Result<()> {
 					continue;
 				}
 				if dict.contains(&word) {
+					map[tries] = word.clone();
+					display_map(answer.unwrap().to_string(), ans_freq, &map);
 					if answer.unwrap() == &word {
 						println!("YOU WIN");
 						break Ok(());
 					}
-				else {
-					display_guesses(answer.unwrap().to_string(), word);
-				}
 				}
 				else {
 					println!("{} '{}' {}", "Word ".yellow(), word.red(), "is not in dictionary".yellow());
 					continue ;
 				}
-				tries -= 1;
-				if tries == 0 {
+				tries += 1;
+				if tries == 6 {
 					println!("THE OPPOSITE OF WIN HAPPENED");
 					break Ok(());
 				}
