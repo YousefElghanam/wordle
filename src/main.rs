0e2e4930a5d3fce79c6bc3d7b80f9ef5b89d3138
YousefElghanam <yousef.elghanam@gmail.com>
2025-11-23 21:49:44 +0100

    DONE: all mandatory logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-use std::{array, io::Read};
+use std::io::Read;
 use std::fs::File;
 use rand_set::RandSet;
 use colored::*;
 use rustyline::{DefaultEditor, Result, error::ReadlineError};
 
+const WHITE: i8 = 0;
+const GREEN: i8 = 1;
+const YELLOW: i8 = 2;
 
 fn  load_dict() -> RandSet<String> {
 	let mut	dict: RandSet<String> = RandSet::new();
@@ -42,28 +45,63 @@ fn	fill_ans_freq (answer: String, mut ans_freq: [i32; 26]) -> [i32; 26] {
 	ans_freq
 }
 
-fn	print_ans_freq (ans_freq: [i32; 26]) {
-	for (i, freq) in ans_freq.iter().enumerate() {
-		println!("{}: {}", (b'a' + i as u8) as char, freq)
+fn	print_colorized_word(answer: &String, mut ans_freq: [i32; 26], word: &(String, [i8; 5])) {
+	let mut	cpy = word.clone();
+	
+	for (i, c) in cpy.0.char_indices() {
+		if answer.as_bytes()[i] == c as u8 {
+			cpy.1[i] = GREEN;
+			ans_freq[((c as u8).to_ascii_lowercase() - b'a') as usize] -= 1;
+		}
 	}
+	for (i, c) in cpy.0.char_indices() {
+		if answer.contains(c) && ans_freq[((c as u8).to_ascii_lowercase() - b'a') as usize] > 0 {
+			if cpy.1[i] == WHITE {
+				cpy.1[i] = YELLOW;
+			}
+		}
+	}
+	print!("  ");
+	for (i, c) in cpy.0.char_indices() {
+		if cpy.1[i] == GREEN {
+			print!("{}", c.to_string().green());
+		}
+		else if cpy.1[i] == YELLOW {
+			print!("{}", c.to_string().yellow());
+		}
+		else {
+			print!("{}", c.to_string());
+		}
+	}
+	print!("\n");
 }
 
 fn	display_map(answer: String, ans_freq: [i32; 26], map: &Vec<String>) {
+	let mut	word: (String, [i8; 5]) = (String::new(), [0; 5]);
+	let mut	x = 1;
+
 	for i in 0..6 {
 		if map[i].is_empty() {
-			println!("_____");
+			if x == 1 {
+				print!(">");
+				x = 0
+			}
+			else {
+				print!(" ");
+			}
+			println!(" _____");
 		}
 		else {
-			println!("{}", map[i]);
+			word.0 = map[i].to_string();
+			print_colorized_word(&answer, ans_freq, &word);
 		}
 	}
 }
-
 
 fn  main() -> Result<()> {
 	let mut	rl = DefaultEditor::new()?;
 	let		dict: RandSet<String>;
-	let		answer;
+	let		answer: Option<&String>;
 	let mut	tries = 0;
 	let mut	ans_freq = [0; 26];
 	let mut	map = vec![String::new(); 6];
@@ -81,9 +119,10 @@ fn  main() -> Result<()> {
 	/* DEBUG */
 	println!("looking for {}. shshshs :D", answer.unwrap().green());
 
+	print!("\x1B[2J\x1B[1;1H");
 	display_map(answer.unwrap().to_string(), ans_freq, &map);
 	loop {
-		let	readline = rl.readline(&format!("{}", ">> ".green().bold()));
+		let	readline = rl.readline(&format!("{}", "> ".green().bold()));
 
 		match readline {
 			Ok(word) => {
@@ -98,8 +137,10 @@ fn  main() -> Result<()> {
 					println!("{}", "Word needs to be 5 characters long".yellow());
 					continue;
 				}
+                let _ = rl.add_history_entry(word.trim());
 				if dict.contains(&word) {
 					map[tries] = word.clone();
+					print!("\x1B[2J\x1B[1;1H");
 					display_map(answer.unwrap().to_string(), ans_freq, &map);
 					if answer.unwrap() == &word {
 						println!("YOU WIN");
@@ -107,7 +148,7 @@ fn  main() -> Result<()> {
 					}
 				}
 				else {
-					println!("{} '{}' {}", "Word ".yellow(), word.red(), "is not in dictionary".yellow());
+					println!("{} '{}' {}", "Word".yellow(), word.red(), "is not in dictionary".yellow());
 					continue ;
 				}
 				tries += 1;
